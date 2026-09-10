8e5edf555774771cc7bbb194855e1c33f235a97d
ThanhTien91 <vttien.marvelous.com@gmail.com>
2026-09-10 21:16:50 +0700

    Sua loi Final Drift do tich luy sai so bang Leaky Integrator

--- decoder/position_compensator.m
+++ decoder/position_compensator.m
@@ -7,11 +7,16 @@ function theta_comp = position_compensator(theta_raw, missing_count, PPR)
     % --- 1. CHUẨN HÓA DỮ LIỆU ---
     theta_raw     = theta_raw(:);
     missing_count = missing_count(:);
+    dp_rad        = 2 * pi / (PPR * 4);
 
-    % --- 2. TÍNH TOÁN BÙ TRỪ ---
-    dp_rad           = 2 * pi / (PPR * 4);
-    correction_count = cumsum(missing_count);
+    % --- VÁ LỖI: ÁP DỤNG LEAKY INTEGRATOR ---
+    N = length(missing_count);
+    correction_count = zeros(N, 1);
+    leak_factor = 0.9995; % Trữ 99.95% giá trị bù trừ, xả từ từ để chống trôi
     
-    theta_comp       = theta_raw + correction_count * dp_rad;
+    for i = 2:N
+        correction_count(i) = correction_count(i-1) * leak_factor + missing_count(i);
+    end
     
+    theta_comp = theta_raw + correction_count * dp_rad;
 end
